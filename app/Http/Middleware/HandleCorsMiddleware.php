1c5b221a31ec7bfe5c34ff6c8b9b548cf99dac8a
rakibmiah99 <rakibmiah120199@gmail.com>
2024-05-09 11:08:56 +0600

    dfj

--- app/Http/Middleware/HandleCorsMiddleware.php
+++ app/Http/Middleware/HandleCorsMiddleware.php
@@ -17,8 +17,8 @@ class HandleCorsMiddleware
     {
         $response =  $next($request);
         $response->headers->set('Access-Control-Allow-Origin', '*');
-        $response->headers->set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-        $response->headers->set('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+        $response->headers->set('Access-Control-Allow-Methods', 'POST, GET, OPTIONS, PUT, DELETE');
+        $response->headers->set('Access-Control-Allow-Headers', 'Content-Type, X-Auth-Token, Origin, Authorization');
         return $response;
     }
 }
